53b1b0badc87726036a54b29cafa313b398ba414
guta307 <veiga187@gmail.com>
2024-01-16 16:02:31 -0300

    chore: Reorganize color definitions for their respective purposes
    In this commit, I have reorganized the color definitions in the configuration file to better align with their intended usage within our application. Colors have been grouped and named according to their roles, making the codebase more organized and maintainable.
    
    - Grouped text and background colors under 'theme.textColor' and 'theme.backgroundColor' respectively.
    - Created specific color variants for 'primary', 'secondary', 'error', 'success', 'information', and 'Alert'.
    - Organized gray shades for both dark and light modes.
    - Refactored border colors for consistency.
    
    These changes improve code readability and maintainability, making it easier to manage our color palette effectively.
    
    #chore #colors #refactor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,74 +7,115 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      xs: "12px",
+      sm: "14px",
+      md: "16px",
+      lg: "20px",
+      xl: "32px",
+    },
     extend: {
-      colors: {
-        /*Brand colors*/
+      textColor: {
+        gray: {
+          dark: {
+            primary: "--dark-gray-primary",
+            secondary: "--dark-gray-secondary",
+            tertiary: "--dark-gray-tertiary",
+            quartenary: "--dark-gray-quartenary",
+          },
+          light: {
+            secondary: "--light-gray-secondary",
+          },
+        },
+        primary: {
+          default: "--primary",
+          lighter: "--primary-lighter",
+        },
+        error: {
+          dark: "--dark-red",
+        },
+        sucess: {
+          dark: "--dark-green",
+        },
+        information: {
+          dark: "--dark-blue",
+        },
+      },
 
-        primary: "--primary",
-        primary_darker: "--primary-darker",
-        primary_lighter: "--primary-lighter",
+      backgroundColor: {
+        primary: {
+          default: "--primary",
+          darker: "--primary-darker",
+          lighter: "--primary-lighter",
+        },
+        secondary: {
+          default: "--secondary",
+          darker: "--secondary-darker",
+          lighter: "--secondary-lighter",
+        },
+        error: {
+          dark: "--dark-red",
+          semidark: "--semidark-red",
+          medium: "--medium-red",
+          light: "--light-red",
+        },
+        sucess: {
+          dark: "--dark-green",
+          semidark: "--semidark-green",
+          medium: "--medium-green",
+          light: "--light-green",
+        },
+        information: {
+          dark: "--dark-blue",
+          semidark: "--semidark-blue",
+          medium: "--medium-blue",
+          light: "--light-blue",
+        },
+        Alert: {
+          dark: "--dark-yellow",
+          semidark: "--semidark-yellow",
+          medium: "--medium-yellow",
+          light: "--light-yellow",
+        },
+        gray: {
+          dark: {
+            secondary: "--dark-gray-secondary",
+            quartenary: "--dark-gray-quartenary",
+          },
+          light: {
+            tertiary: "--light-gray-tertiary",
+            quartenary: "--light-gray-quartenary",
+            quintenary: "--dark-gray-quintenary",
+          },
+        },
+      },
 
-        secondary: "--secondary",
-        secondary_darker: "--secondary-darker",
-        secondary_lighter: "--secondary-lighter",
-
-        /*--Brand colors--*/
-
-        /*Text & Background*/
-
-        /*Dark gray*/
-        dark_gray_primary: "--dark-gray-primary",
-        dark_gray_secondary: "--dark-gray-secondary",
-        dark_gray_tertiary: "--dark-gray-tertiary",
-        dark_gray_quartenary: "--dark-gray-quartenary",
-        dark_gray_quintenary: "--dark-gray-quintenary",
-        /*--Dark gray--*/
-
-        /*Light gray*/
-        light_gray_primary: "--light-gray-primary",
-        light_gray_secondary: "--light-gray-secondary",
-        light_gray_tertiary: "--light-gray-tertiary",
-        light_gray_quartenary: "--light-gray-quartenary",
-        light_gray_quintenary: "--light-gray-quintenary",
-        /*--Light gray--*/
-        /*--Text & Background--*/
-
-        /*Custom colors*/
-
-        /*Red*/
-        dark_red: "--dark-red",
-        semidark_red: "--semidark-red",
-        medium_red: "--medium-red",
-        semilight_red: "--semilight-red",
-        light_red: "--light-red",
-        /*--Red--*/
-
-        /*green*/
-        dark_green: "--dark-green",
-        semidark_green: "--semidark-green",
-        medium_green: "--medium-green",
-        semilight_green: "--semilight-green",
-        light_green: "--light-green",
-        /*--green--*/
-
-        /*yellow*/
-        dark_yellow: "--dark-yellow",
-        semidark_yellow: "--semidark-yellow",
-        medium_yellow: "--medium-yellow",
-        semilight_yellow: "--semilight-yellow",
-        light_yellow: "--light-yellow",
-        /*--yellow--*/
-
-        /*blue*/
-        dark_blue: "--dark-blue",
-        semidark_blue: "--semidark-blue",
-        medium_blue: "--medium-blue",
-        semilight_blue: "--semilight-blue",
-        light_blue: "--light-blue",
-        /*--blue--*/
-
-        /*--Custom colors--*/
+      borderColor: {
+        gray: {
+          dark: {
+            tertiary: "--dark-gray-tertiary",
+            quartenary: "--dark-gray-quartenary",
+          },
+          light: {
+            tertiary: "--light-gray-tertiary",
+            quartenary: "--light-gray-quartenary",
+          },
+        },
+        primary: {
+          outline: "--primary-outline",
+        },
+        secondary: {
+          default: "--secondary",
+        },
+        information: {
+          dark: "--dark-blue",
+        },
+        sucess: {
+          dark: "--dark-green",
+        },
+        Alert: {
+          dark: "--dark-yellow",
+        },
       },
     },
   },
