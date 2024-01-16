66c107a47cf5dd119dfde8446e1eb28348bc7a14
guta307 <veiga187@gmail.com>
2024-01-16 11:24:00 -0300

    docs(storybook): Add paths for molecules and atoms components stories

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/components/molecules/**/*.mdx",
+    "../src/components/molecules/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/components/atoms/**/*.mdx",
+    "../src/components/atoms/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
